ab4a1676296605c84e34539d4506bacf7b81ac83
UBaksa <ujkanovicbakir03@gmail.com>
2023-11-07 16:25:31 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Homepage from "./pages/Homepage/Homepage";
+function App() {
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
